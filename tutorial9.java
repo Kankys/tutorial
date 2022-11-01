4d4976cc265fd9e924470038c2bd746801e69f27
Lukáš Kaňka <73284786+Kankys@users.noreply.github.com>
2022-11-01 20:11:49 +0100

    Add files via upload

---
+++ tutorial9.java
@@ -0,0 +1,22 @@
+package tutorial.lukan;
+// Vstup od uživatele
+// Zde použijeme scanner
+import java.util.Scanner;
+
+public class tutorial9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String retezec;
+        System.out.println("Zadejte řetězec");
+        retezec = in.nextLine(); // přiřadí do proměnné hodnotu kterou uživatel napíše
+        System.out.println("Uživatel zadal: " + retezec);
+
+
+
+
+    }
+
+}
+
+
+
